e149d28c9ab4bafd5e824feb15c55ceed3b47592
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-10-16 23:07:06 -0300

    Add Button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,26 @@
+import React, { FunctionComponent, HTMLProps } from "react";
+import classNames from "classnames";
+import styles from "./styles.module.scss";
+
+export const Button: FunctionComponent<IButtonProps> = (
+  {
+    className,
+    children,
+    ...props
+  }
+) => (
+  <button
+    className={classNames(styles.main, styles.text, styles.color, styles.fitContent, className)}
+    {...props}
+  >
+    {children}
+  </button>
+);
+
+export interface IButtonProps extends HTMLProps<HTMLButtonElement> {
+  className?: string;
+  onClick?: (state: object) => void;
+  disabled?: boolean;
+  autoFocus?: boolean;
+  type?: "button" | "submit" | "reset" | undefined;
+}
