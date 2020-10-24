c331fe3b635473b50241563fab2691280114d07a
nereapiccone <nerenap@gmail.com>
2020-10-24 19:05:47 -0300

    Agregado de bordes

--- src/components/VolunteersTable/VolunteerItem/index.tsx
+++ src/components/VolunteersTable/VolunteerItem/index.tsx
@@ -1,9 +1,10 @@
 import React, { FunctionComponent } from "react";
 import { IVolunteer } from "$hooks/Volunteers";
+import styles from "./styles.module.scss";
 
 export const VolunteerItem: FunctionComponent<IVolunteer> =
     ({firstName, surname, dni, commission}) => {
-        return <tr>
+        return <tr className={styles.tableItem}>
             <td>
                 <h5>{dni}</h5>
             </td>
