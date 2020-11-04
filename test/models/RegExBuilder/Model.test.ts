df865c99dcf3238c49921e3b3862e0d8887b6e5c
Lautaro <lautarodelosheros@gmail.com>
2020-11-04 12:48:28 -0300

    Refactor RegExBuilder tests

--- test/models/RegExBuilder/Model.test.ts
+++ test/models/RegExBuilder/Model.test.ts
@@ -1,22 +1,57 @@
 import { RegExBuilder } from "$models/RegExBuilder";
 
-describe("RegExBuilder", () => {
-  it("validates different phone numbers formats", () => {
+describe("RegExBuilder positive test", () => {
+  it("validates 8 digits phone numbers", () => {
     expect("12345678").toMatch(RegExBuilder.phoneNumber());
-    expect("01123456789").toMatch(RegExBuilder.phoneNumber());
-    expect("1123456789").toMatch(RegExBuilder.phoneNumber());
-    expect("541123456789").toMatch(RegExBuilder.phoneNumber());
-    expect("5491123456789").toMatch(RegExBuilder.phoneNumber());
-    expect("+5491123456789").toMatch(RegExBuilder.phoneNumber());
-    expect("91123456789").toMatch(RegExBuilder.phoneNumber());
-    expect("+19142329901").toMatch(RegExBuilder.phoneNumber());
-    expect("+442087599036").toMatch(RegExBuilder.phoneNumber());
   });
 
-  it("validates invalid phone numbers formats for negative output", () => {
-    expect("a12345678".match(RegExBuilder.phoneNumber())).toBeFalsy();
-    expect("1234-5678".match(RegExBuilder.phoneNumber())).toBeFalsy();
-    expect("12345678a".match(RegExBuilder.phoneNumber())).toBeFalsy();
-    expect("++5412345678".match(RegExBuilder.phoneNumber())).toBeFalsy();
+  it("validates 8 digits phone numbers with area code", () => {
+    expect("01123456789").toMatch(RegExBuilder.phoneNumber());
+  });
+
+  it("validates 10 digits phone numbers", () => {
+    expect("1123456789").toMatch(RegExBuilder.phoneNumber());
+  });
+
+  it("validates 10 digits phone numbers with country code", () => {
+    expect("541123456789").toMatch(RegExBuilder.phoneNumber());
+  });
+
+  it("validates 10 digits phone numbers with area and country code", () => {
+    expect("5491123456789").toMatch(RegExBuilder.phoneNumber());
+  });
+
+  it("validates 10 digits phone numbers with area and country code and plus sign", () => {
+    expect("+5491123456789").toMatch(RegExBuilder.phoneNumber());
+  });
+
+  it("validates 10 digits phone numbers with area code", () => {
+    expect("91123456789").toMatch(RegExBuilder.phoneNumber());
+  });
+
+  it("validates USA standard phone numbers with country code and plus sign", () => {
+    expect("+19142329901").toMatch(RegExBuilder.phoneNumber());
+  });
+
+  it("validates UK standard phone numbers with country code and plus sign", () => {
+    expect("+442087599036").toMatch(RegExBuilder.phoneNumber());
+  });
+});
+
+describe("RegExBuilder negative test", () => {
+  it("returns false if the value contains a leading invalid character", () => {
+    expect(RegExBuilder.phoneNumber().test("a12345678")).toBe(false);
+  });
+
+  it("returns false if the value contains a hyphen", () => {
+    expect(RegExBuilder.phoneNumber().test("1234-5678")).toBe(false);
+  });
+
+  it("returns false if the value contains a traling invalid character", () => {
+    expect(RegExBuilder.phoneNumber().test("12345678a")).toBe(false);
+  });
+
+  it("returns false if the value contains a duplicated plus sign", () => {
+    expect(RegExBuilder.phoneNumber().test("++5412345678")).toBe(false);
   });
 });
