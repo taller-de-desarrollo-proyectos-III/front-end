901d5659a9c6a7c2c2bb2219547346ff8f92d12f
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-11-04 14:21:26 -0300

    Add Title component

--- src/pages/CreateVolunteer/component.tsx
+++ src/pages/CreateVolunteer/component.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { FunctionComponent } from "react";
 import { MainContent } from "$components/MainContent";
+import { Title } from "$components/Title";
 import { CreateVolunteerForm, ICreateVolunteerFormProps } from "$components/CreateVolunteerForm";
 import { NavBar } from "$components/NavBar";
-import styles from "./styles.module.scss";
 
 export const CreateVolunteer: FunctionComponent<ICreateVolunteerFormProps> = formValues => (
   <MainContent>
     <NavBar />
-    <h1 className={styles.title}>Crear Voluntarie</h1>
+    <Title title={"Crear Voluntarie"} />
     <CreateVolunteerForm {...formValues} />
   </MainContent>
 );
