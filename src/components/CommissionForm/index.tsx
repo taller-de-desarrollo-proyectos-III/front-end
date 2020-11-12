0b61cbdc5aaa70ee35bef5300dc2f143e62f6b5c
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-11-12 13:20:35 -0300

    Add CommissionForm

---
+++ src/components/CommissionForm/index.tsx
@@ -0,0 +1,45 @@
+import React, { FunctionComponent } from "react";
+import { Dialog, DialogActions, DialogContent, DialogTitle } from "@material-ui/core";
+import { Form, Formik } from "formik";
+import { Button } from "$components/Button";
+import styles from "./styles.module.scss";
+import { TextField } from "../TextField";
+
+const formName = "CommissionForm";
+
+export const CommissionForm: FunctionComponent<IConfirmDialogProps> = ({
+  isOpen,
+  onClose,
+  initialValues,
+  onSubmit
+}) => (
+  <Dialog open={isOpen} onClose={onClose}>
+    <DialogTitle>
+      <span className={styles.title}>{"title"}</span>
+    </DialogTitle>
+    <DialogContent>
+      <Formik initialValues={initialValues} onSubmit={onSubmit}>
+        {({ isSubmitting }) => (
+          <Form id={formName}>
+            <TextField name="name" label="Nombre" disabled={isSubmitting} fullWidth />
+          </Form>
+        )}
+      </Formik>
+    </DialogContent>
+    <DialogActions>
+      <Button onClick={onClose} kind="secondary">
+        Cancelar
+      </Button>
+      <Button form={formName} kind="primary">
+        Confirmar
+      </Button>
+    </DialogActions>
+  </Dialog>
+);
+
+interface IConfirmDialogProps {
+  isOpen: boolean;
+  onClose: () => void;
+  initialValues: { name: string };
+  onSubmit: (values: { name: string }) => void;
+}
