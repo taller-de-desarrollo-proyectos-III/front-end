01281ccc8c0cb446106749c2e3d527082d11bbac
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-12-08 02:00:18 -0300

    Move serializers to Serializers directory

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,4 @@
 export * from "./RoutesBuilder";
 export * from "./RegExBuilder";
 export * from "./nullToUndefined";
-export * from "./VolunteerSerializer";
-export * from "./CommissionsSerializer";
-export * from "./RolesSerializer";
+export * from "./Serializers";
