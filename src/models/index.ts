689ba425036d64a029c3af6875dfbc3d64ed2f2d
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-12-08 01:53:52 -0300

    Add CommissionsSerializer

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,3 +2,4 @@ export * from "./RoutesBuilder";
 export * from "./RegExBuilder";
 export * from "./nullToUndefined";
 export * from "./VolunteerSerializer";
+export * from "./CommissionsSerializer";
