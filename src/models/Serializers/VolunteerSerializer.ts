b09f61004a9c7c033221dda3495985f6fe8747da
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-12-08 02:23:53 -0300

    Use CommissionsSerializer and RolesSerializer in the VolunteerSerializer

--- src/models/Serializers/VolunteerSerializer.ts
+++ src/models/Serializers/VolunteerSerializer.ts
@@ -1,11 +1,13 @@
 import { IVolunteer } from "$hooks";
+import { RolesSerializer } from "./RolesSerializer";
+import { CommissionsSerializer } from "./CommissionsSerializer";
 
 export const VolunteerSerializer = {
   serialize: ({ uuid, commissions, roles, ...attributes }: IVolunteer) => {
     return {
       ...attributes,
-      commissions: commissions.map(commission => commission.name).join(","),
-      roles: roles.map(role => role.name).join(",")
+      commissions: CommissionsSerializer.serialize(commissions),
+      roles: RolesSerializer.serialize(roles)
     };
   }
 };
