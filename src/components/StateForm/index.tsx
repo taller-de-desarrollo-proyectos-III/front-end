7d967421adba2f61748519962d8898e8562da4ce
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2021-02-04 20:47:34 -0300

    Use DialogForm in the StateForm

--- src/components/StateForm/index.tsx
+++ src/components/StateForm/index.tsx
@@ -1,11 +1,5 @@
 import React, { FunctionComponent } from "react";
-import { Dialog, DialogActions, DialogContent, DialogTitle } from "@material-ui/core";
-import { Form, Formik } from "formik";
-import { Button } from "$components/Button";
-import styles from "./styles.module.scss";
-import { TextField } from "../TextField";
-
-const formName = "StateForm";
+import { DialogForm } from "$components/DialogForm";
 
 export const StateForm: FunctionComponent<IConfirmDialogProps> = ({
   isOpen,
@@ -13,28 +7,14 @@ export const StateForm: FunctionComponent<IConfirmDialogProps> = ({
   initialValues,
   onSubmit
 }) => (
-  <Dialog open={isOpen} onClose={onClose}>
-    <DialogTitle>
-      <span className={styles.title}>{"Escriba el nombre del estado"}</span>
-    </DialogTitle>
-    <DialogContent>
-      <Formik initialValues={initialValues} onSubmit={onSubmit}>
-        {({ isSubmitting }) => (
-          <Form id={formName}>
-            <TextField name="name" label="Nombre" disabled={isSubmitting} fullWidth />
-          </Form>
-        )}
-      </Formik>
-    </DialogContent>
-    <DialogActions>
-      <Button onClick={onClose} kind="secondary">
-        Cancelar
-      </Button>
-      <Button form={formName} kind="primary" type="submit">
-        Confirmar
-      </Button>
-    </DialogActions>
-  </Dialog>
+  <DialogForm
+    withDescription={false}
+    title="Escriba el nombre del estado"
+    isOpen={isOpen}
+    onClose={onClose}
+    initialValues={initialValues}
+    onSubmit={onSubmit}
+  />
 );
 
 interface IConfirmDialogProps {
