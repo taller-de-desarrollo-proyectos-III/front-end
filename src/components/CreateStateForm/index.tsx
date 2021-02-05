9c1b4bccacf4b7075f257f130cbeba84f0a2b528
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2021-02-04 23:05:16 -0300

    Use CreateOrUpdateFormDialog in the States page

--- src/components/CreateStateForm/index.tsx
+++
@@ -1,1 +0,0 @@
-export { CreateStateFormContainer as CreateStateForm } from "./container";
--- src/pages/States/component.tsx
+++ src/pages/States/component.tsx
@@ -1,30 +1,41 @@
 import React, { FunctionComponent } from "react";
-import { IState } from "$hooks";
+import { IState, useCreateState } from "$hooks";
 import { StatesTable } from "$components/StatesTable";
 import { NavBar } from "$components/NavBar";
 import { Title } from "$components/Title";
 import { MainContent } from "$components/MainContent";
 import { Button } from "$components/Button";
-import { CreateStateForm } from "$components/CreateStateForm";
+import { CreateOrUpdateFormDialog } from "$components/CreateOrUpdateFormDialog";
 import styles from "./styles.module.scss";
 
 export const States: FunctionComponent<IComponentProps> = ({
   states,
   setOpenStateForm,
   openStateForm
-}) => (
-  <MainContent>
-    <NavBar />
-    <div className={styles.body}>
-      <Title className={styles.title} title={"Listado de estados"} />
-      <Button className={styles.addState} onClick={() => setOpenStateForm(true)}>
-        Agregar
-      </Button>
-      <StatesTable className={styles.table} states={states} />
-      <CreateStateForm isOpen={openStateForm} onClose={() => setOpenStateForm(false)} />
-    </div>
-  </MainContent>
-);
+}) => {
+  const { createState } = useCreateState();
+  return (
+    <MainContent>
+      <NavBar />
+      <div className={styles.body}>
+        <Title className={styles.title} title={"Listado de estados"} />
+        <Button className={styles.addState} onClick={() => setOpenStateForm(true)}>
+          Agregar
+        </Button>
+        <StatesTable className={styles.table} states={states} />
+        <CreateOrUpdateFormDialog
+          title="Escriba el nombre del estado"
+          isOpen={openStateForm}
+          onClose={() => setOpenStateForm(false)}
+          withDescription
+          initialValues={{ name: "" }}
+          update={false}
+          createOrUpdate={createState}
+        />
+      </div>
+    </MainContent>
+  );
+};
 
 interface IComponentProps {
   states?: IState[];
