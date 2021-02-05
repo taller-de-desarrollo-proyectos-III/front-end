eee0a8269156791408b99fb74b2081d643603f68
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2021-02-04 22:56:21 -0300

    Use CreateOrUpdateFormDialog in the Roles page

--- src/components/CreateRoleForm/index.tsx
+++
@@ -1,1 +0,0 @@
-export { CreateRoleFormContainer as CreateRoleForm } from "./container";
--- src/pages/Roles/component.tsx
+++ src/pages/Roles/component.tsx
@@ -1,30 +1,42 @@
 import React, { FunctionComponent } from "react";
-import { IRole } from "$hooks";
+import { IRole, useCreateRole } from "$hooks";
 import { RolesTable } from "$components/RolesTable";
 import { NavBar } from "$components/NavBar";
 import { Title } from "$components/Title";
 import { MainContent } from "$components/MainContent";
 import { Button } from "$components/Button";
-import { CreateRoleForm } from "$components/CreateRoleForm";
+import { CreateOrUpdateFormDialog } from "$components/CreateOrUpdateFormDialog";
 import styles from "./styles.module.scss";
 
 export const Roles: FunctionComponent<IComponentProps> = ({
   roles,
   setOpenRoleForm,
   openRoleForm
-}) => (
-  <MainContent>
-    <NavBar />
-    <div className={styles.body}>
-      <Title className={styles.title} title={"Listado de roles"} />
-      <Button className={styles.addRole} onClick={() => setOpenRoleForm(true)}>
-        Agregar
-      </Button>
-      <RolesTable className={styles.table} roles={roles} />
-      <CreateRoleForm isOpen={openRoleForm} onClose={() => setOpenRoleForm(false)} />
-    </div>
-  </MainContent>
-);
+}) => {
+  const { createRole } = useCreateRole();
+
+  return (
+    <MainContent>
+      <NavBar />
+      <div className={styles.body}>
+        <Title className={styles.title} title={"Listado de roles"} />
+        <Button className={styles.addRole} onClick={() => setOpenRoleForm(true)}>
+          Agregar
+        </Button>
+        <RolesTable className={styles.table} roles={roles} />
+        <CreateOrUpdateFormDialog
+          title="Escriba el nombre y la descripción del rol"
+          isOpen={openRoleForm}
+          onClose={() => setOpenRoleForm(false)}
+          withDescription
+          initialValues={{ name: "", description: "" }}
+          update={false}
+          createOrUpdate={createRole}
+        />
+      </div>
+    </MainContent>
+  );
+};
 
 interface IComponentProps {
   roles?: IRole[];
