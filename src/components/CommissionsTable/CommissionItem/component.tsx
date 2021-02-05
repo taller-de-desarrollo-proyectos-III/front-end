09b3cc951b945330b0dcce653fe3914fecab52af
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2021-02-04 23:00:10 -0300

    Use CreateOrUpdateFormDialog in the CommissionItem

--- src/components/CommissionsTable/CommissionItem/component.tsx
+++ src/components/CommissionsTable/CommissionItem/component.tsx
@@ -1,8 +1,9 @@
 import React, { FunctionComponent } from "react";
-import { EditCommissionForm } from "$components/EditCommissionForm";
+import { useUpdateCommission } from "$hooks";
 import { TableRow } from "$components/Table/TableRow";
 import { ActionItem } from "$components/Table/ActionItem";
 import { TableItem } from "$components/Table/TableItem";
+import { CreateOrUpdateFormDialog } from "$components/CreateOrUpdateFormDialog";
 import { IComponentProps } from "./interfaces";
 
 export const CommissionItem: FunctionComponent<IComponentProps> = ({
@@ -10,19 +11,26 @@ export const CommissionItem: FunctionComponent<IComponentProps> = ({
   isOpen,
   setIsOpen,
   readonly
-}) => (
-  <>
-    <TableRow>
-      <TableItem>{commission.name}</TableItem>
-      <TableItem>{commission.description || "No tiene"}</TableItem>
-      {!readonly && <ActionItem onClick={() => setIsOpen(true)}>Editar</ActionItem>}
-    </TableRow>
-    {!readonly && (
-      <EditCommissionForm
-        initialValues={commission}
-        isOpen={isOpen}
-        onClose={() => setIsOpen(false)}
-      />
-    )}
-  </>
-);
+}) => {
+  const { updateCommission } = useUpdateCommission();
+  return (
+    <>
+      <TableRow>
+        <TableItem>{commission.name}</TableItem>
+        <TableItem>{commission.description || "No tiene"}</TableItem>
+        {!readonly && <ActionItem onClick={() => setIsOpen(true)}>Editar</ActionItem>}
+      </TableRow>
+      {!readonly && (
+        <CreateOrUpdateFormDialog
+          title="Escriba el nombre y la descripción de la comisión"
+          isOpen={isOpen}
+          onClose={() => setIsOpen(false)}
+          withDescription
+          initialValues={commission}
+          update={true}
+          createOrUpdate={updateCommission}
+        />
+      )}
+    </>
+  );
+};
--- src/components/EditCommissionForm/index.ts
+++
@@ -1,1 +0,0 @@
-export { EditCommissionFormContainer as EditCommissionForm } from "./container";
